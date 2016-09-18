497344d88ebcb260e08998f6028f0758901e0205
Sven Popping <sven@popping.co>
2016-09-19 01:45:36 +0200

    Add frontend ticket buy process logic

--- js/ticket.js
+++ js/ticket.js
@@ -2,15 +2,22 @@
  * Created by sven on 16/09/2016.
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
+    // Localstorage
+    var ticketdb = localStorage.getItem('ticketdatabase');
+    var tickets = (null == ticketdb || "" == ticketdb) ? [] : JSON.parse(ticketdb);
+
     // Blueprint
-    var blueprint = $("#blueprint").html();
+    var blueprint = $('#blueprint').html();
     $("#blueprint").remove();
+    var tableRow = "<tr><td>%ticketNumber%</td><td><h4>%ticketName%</h4><span>%name%" +
+        " &lt;%email%&gt;</span></td><td>%amount%</td></tr>";
 
-    $.ajax("https://lustrum.ch/api/ticket/get/all", {
+    //
+    updateBasket()
 
-    }).always(function(e, r) {
-        $.each(e, function(k, v) {
+    $.ajax("https://lustrum.ch/api/ticket/get/all").always(function (e, r) {
+        $.each(e, function (k, v) {
             var ticket = blueprint;
             v.date = (new Date(v.date)).toLocaleDateString("nl-NL", {
                 day: "2-digit",
@@ -18,11 +25,92 @@ $(document).ready(function() {
                 year: "numeric"
             });
             v.amount = "&euro;" + v.amount.toFixed(2).replace(".", ",");
-            $.each(v, function(key, value) {
+            $.each(v, function (key, value) {
                 ticket = ticket.split("%" + key + "%").join(value);
             });
 
             $("#ticketContainer").append(ticket);
         })
     });
+
+    $('#ticketModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var key = button.data('key');
+        var name = button.data('ticket');
+        var amount = button.data('amount');
+
+        var model = $(this);
+        model.find("#ticketKey").val(key);
+        model.find("#ticketName").val(name);
+        model.find("#ticketAmount").val(amount);
+    });
+
+    $('#addTicketToBasket').click(function() {
+        var name = $('#ticketUserName').val();
+        var email = $('#ticketEmail').val();
+        var key = $('#ticketKey').val();
+        var ticketName = $('#ticketName').val();
+        var amount = $('#ticketAmount').val();
+
+        tickets.push({
+            name: name,
+            email: email,
+            key: key,
+            ticketName: ticketName,
+            amount: amount
+        });
+        localStorage.setItem('ticketdatabase', JSON.stringify(tickets));
+
+        // Hide modal and update basket badge
+        $('#ticketModal').modal('hide');
+        updateBasket();
+    });
+
+    $('#toCheckout').on('click', function() {
+        $.ajax("https://lustrum.ch/api/order/create", {
+            method: "POST",
+            data: {
+                data: JSON.parse(localStorage.getItem('ticketdatabase'))
+            }
+        }).done(function(e, r) {
+            var json = JSON.parse(e);
+            if (200 == json.status) {
+                localStorage.setItem('ticketdatabase', []);
+                var count = 5;
+                setInterval(function(){
+                    $('#checkTimeOut').show();
+                    $('#checkTimeOut').html("You will be redirected to iDeal in " + count + " seconds. Or <a href='" + json.redirect_url +"'>click" +
+                        " here</a> if nothing happens.");
+                    count--;
+
+                    if (count < 0) {
+                        $('#checkTimeOut').hide();
+                        window.location.replace(json.redirect_url);
+                    }
+                },1000);
+            }
+        });
+    });
+
+    function updateBasket() {
+        //Badge
+        $("#ticketBadge").html(tickets.length);
+
+        // Basket
+        var basket = $("#basketBody");
+        var sum = 0;
+        basket.html("");
+        $.each(tickets, function(k, v) {
+            var ticket = tableRow;
+            sum += parseInt(v.amount.split("€")[1]);
+            v.ticketNumber = k + 1;
+            $.each(v, function (key, value) {
+                ticket = ticket.split("%" + key + "%").join(value);
+            });
+            basket.append(ticket);
+        });
+
+        // Total
+        $('#tableTotal').html(sum.toFixed(2).replace(".", ","));
+    }
 });
--- pages/tickets.php
+++ pages/tickets.php
@@ -11,15 +11,95 @@
 
     <section>
         <div class="container ticket-container">
+            <div class="row">
+                <div class="col-sm-offset-11 col-sm-1">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#titleBasket">
+                        <i class="fa fa-shopping-cart fa-fw" aria-hidden="true"></i> Basket
+                        <span class="badge" id="ticketBadge"></span>
+                    </button>
+                </div>
+            </div>
+
+
             <div class="row" id="ticketContainer">
 
             </div>
         </div>
+
+        <div class="modal fade" id="ticketModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="modalLabel">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="tickeUsertName">Name</label>
+                            <input type="text" class="form-control" id="ticketUserName" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="ticketEmail">Email address</label>
+                            <input type="email" class="form-control" id="ticketEmail" placeholder="Email">
+                        </div>
+
+                        <input type="hidden" id="ticketKey">
+                        <input type="hidden" id="ticketName">
+                        <input type="hidden" id="ticketAmount">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="addTicketToBasket">
+                            Add ticket to basket!
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="titleBasket" tabindex="-1" role="dialog" aria-labelledby="basketLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="basketLabel">Lustrum #trending - basket</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="alert alert-success" role="alert" id="checkTimeOut" hidden></div>
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                            </tr>
+                            </thead>
+                            <tfoot>
+                            <tr>
+                                <th></th>
+                                <th style="text-align: right;">Total</th>
+                                <th>&euro; <span id="tableTotal"></span></th>
+                            </tr>
+                            </tfoot>
+                            <tbody id="basketBody" class="basket-table"></tbody>
+                        </table>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="toCheckout">
+                            Checkout
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <div id="blueprint">
+    <div id="blueprint" style="display: none;">
         <div class="col-sm-6 col-md-4 ticket">
-            <div class="hidden-xs col-sm-3 ticket-images"></div>
+            <div class="hidden-xs col-sm-3 ticket-images" style="background-image: url('%background%');"></div>
             <div class="col-sm-9 ticket-description">
                 <span class="date">%date%</span>
                 <h1>%name%</h1>
@@ -28,10 +108,15 @@
                     <span class="price">%amount%</span>
                 </div>
             </div>
-            <a href="#" class="btn btn-default ticket-button" data-key="%key%">
-                <i class="fa fa-shopping-cart fa-fw" aria-hidden="true"></i> Add ticket to basket!
+            <a href="#" data-toggle="modal" data-target="#ticketModal" class="btn btn-default ticket-button"
+               data-key="%key%" data-ticket="%name%" data-amount="%amount%">
+                Add ticket to basket!
             </a>
         </div>
+    </div>
+
+    <div id="blueprintTableRow" style="display: none;">
+
     </div>
 
 <?php include_once "components/footer.html" ?>
